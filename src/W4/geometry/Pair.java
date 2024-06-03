7715f1b559437f0e5c2cc7a2814d8be43df4b806
Taemin Lee <taeminlee0601@gmail.com>
2024-06-03 10:51:08 -0400

    Added W4 Notes

---
+++ src/W4/geometry/Pair.java
@@ -0,0 +1,11 @@
+package W4.geometry;
+
+public class Pair<T> {
+    T first;
+    T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+}
